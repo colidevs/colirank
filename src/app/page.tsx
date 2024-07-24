200acded9d8e899ed86ec83ba982706cd4f35446
Ezequiel Mareco <ezequielmareco33@gmail.com>
2024-07-23 22:15:50 -0300

    se agrego espaciado entre los botones
    Co-authored-by: Guillesture <guillesture@gmail.com>
    Co-authored-by: Astrid <astridnaivirt22@outlook.com>
    Co-authored-by: Guadalupe  <guadalupesanchezmtb104@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import {Button} from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <section className="text-center">
+    <section className="space-x-16 text-center">
       <Button>Boton 1</Button>
       <Button>Boton 2</Button>
       <Button>Boton 3</Button>
