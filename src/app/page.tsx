e341df7424ed69b3cd7865deb8f0c32983d858fa
Ezequiel Mareco <ezequielmareco33@gmail.com>
2024-07-30 19:18:45 -0300

    posicionamos los botones y cambiamos los estilos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import {Button} from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <section>
+    <section className="flex flex-row space-x-96">
       <div className="w-4/12">
         <TableDemo />
       </div>
-      <div className="mx-8 space-x-16 text-center">
-        <Button>Boton 1</Button>
-        <Button>Boton 2</Button>
-        <Button>Boton 3</Button>
+      <div className="m-auto w-6/12 space-x-16 text-center">
+        <Button className="bg-blue-400 p-10">Boton 1</Button>
+        <Button className="bg-zinc-200 p-10">Boton 2</Button>
+        <Button className="bg-green-400 p-10">Boton 3</Button>
       </div>
     </section>
   );
