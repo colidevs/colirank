1d3235400b31f6d06d851aceda70de5ce68dff49
Astrid <astridnaivirt22@outlook.com>
2024-08-14 23:10:09 -0300

    cambio emoji header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-zinc-50 bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]" />
         <div className="border-b border-zinc-300 bg-white">
           <header className="ms-32 text-2xl font-bold leading-[4rem]">
-            <Link href="/">colirank 🦄</Link>
+            <Link href="/">colirank 🏆</Link>
           </header>
         </div>
         <div className="container relative z-10 m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4 font-sans antialiased">
