7f3c959a79f7a54cd189f8b43a9ed20d80cf1a17
Astrid <astridnaivirt22@outlook.com>
2024-08-14 22:18:20 -0300

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className="relative h-screen w-screen overflow-hidden bg-slate-900 text-rose-950">
-        <div class="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-zinc-50 bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
-        <div className="bg-white border-b border-zinc-300">
+        <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-zinc-50 bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]" />
+        <div className="border-b border-zinc-300 bg-white">
           <header className="ms-32 text-2xl font-bold leading-[4rem]">
             <Link href="/">colirank 🦄</Link>
           </header>
