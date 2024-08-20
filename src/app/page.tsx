e63d83ce6628fe88a9a2ff879f3348e7f6764ced
pufi <thomas.r.pufahl@gmail.com>
2024-08-20 10:26:31 -0300

    add server side props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import {type User} from "@prisma/client";
+import {GetServerSideProps, InferGetServerSidePropsType} from "next";
 
 import HomePageClient from "./page.client";
 
-import {api} from "@/api";
+import prisma from "@/db";
 import {UserProviderClient} from "@/usersContext";
 
-export default async function HomePage() {
-  const users: User[] = await api.list();
+export const getServerSideProps = (async () => {
+  // Fetch data from external API
+  const users: User[] = await prisma.user.findMany();
 
+  // Pass data to the page via props
+  return {props: {users}};
+}) satisfies GetServerSideProps<{users: User[]}>;
+
+export default function HomePage({users}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <UserProviderClient usersFromDb={users}>
       <HomePageClient />
